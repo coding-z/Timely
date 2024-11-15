a1146f7acf8bf8d618a80230f6aecac901297c96
Zareef <zmas0005@student.monash.edu>
2024-11-15 16:11:02 +1100

    Add toggling of item status

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,42 @@
-// import styles from "./home.module.css";
+"use client";
+
+import { useState } from "react";
 import "./home.css";
 
 export default function Home() {
+  const [list, setList] = useState({
+    title: "Daily Stuff",
+    items: [
+      { id: 1, name: "Exercise", done: true },
+      { id: 2, name: "Sleep", done: true },
+      { id: 3, name: "Work on project", done: false },
+      { id: 4, name: "Do something", done: false },
+      { id: 5, name: "This is a new item", done: false },
+    ],
+  });
+
+  function handleToggleItemStatus(id: number) {
+    setList({
+      ...list,
+      items: list.items.map((item) =>
+        item.id === id ? { ...item, done: !item.done } : item
+      ),
+    });
+  }
+
   return (
     <main className="glass">
-      <h1>List Name</h1>
+      <h1>{list.title}</h1>
       <ul>
-        <li className="done">
-          <p>Exercise</p>
-        </li>
-        <li className="done">
-          <p>Sleep</p>
-        </li>
-        <li>
-          <p>Work on project</p>
-        </li>
-        <li>
-          <p>Do something</p>
-        </li>
+        {list.items.map((item) => (
+          <li
+            key={item.id}
+            className={item.done ? "done" : undefined}
+            onClick={() => handleToggleItemStatus(item.id)}
+          >
+            <p>{item.name}</p>
+          </li>
+        ))}
       </ul>
     </main>
   );
