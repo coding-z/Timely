4946fe29f1300fe8adb4bc219ae4ead2d194b84d
Zareef <zmas0005@student.monash.edu>
2024-11-20 17:50:23 +1100

    Update global styles

--- app/template.tsx
+++ app/template.tsx
@@ -25,6 +25,16 @@ export default function Template({ children }: { children: React.ReactNode }) {
           width: 100vw;
           min-height: 100vh;
           height: 100vh;
+          background-image: linear-gradient(135deg, #f97794 10%, #623aa2 100%);
+          color: rgb(255 255 255 / 0.8);
+
+          * {
+            color: inherit;
+            font-family: inherit;
+            font-optical-sizing: inherit;
+            font-weight: inherit;
+            font-style: inherit;
+          }
         }
 
         h1, h2, h3, h4, h5, h6, p {
