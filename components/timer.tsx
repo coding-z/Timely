be3a59b3f1120212d6395c6aee092e5ccde98bf4
Zareef <zmas0005@student.monash.edu>
2024-11-21 16:36:07 +1100

    Add initial timer input component

--- components/timer.tsx
+++ components/timer.tsx
@@ -1,11 +1,41 @@
 "use client";
 
+import { ChangeEvent, useState } from "react";
 import Button from "./button";
+
+function TimerInput() {
+  const [value, setValue] = useState("00:00:00");
+
+  function handleChange(event: ChangeEvent<HTMLInputElement>) {
+    setValue(event.target.value);
+  }
+  
+  return (
+    <>
+      <input value={value} onChange={handleChange} />
+      <style jsx>{`
+        input {
+          background-color: transparent;
+          font-size: var(--font-size-xl);
+          width: 230.406px;   // TODO: Calculate dynamically?
+          height: 62px;       // ^
+          border: none;
+          padding: 0;
+
+          &:focus {
+            outline: none;
+          }
+        }
+      `}</style>
+    </>
+  );
+}
 
 export default function Timer() {
   return (
     <div className="root">
-      <h1>00:00:00</h1>
+      {/* <h1>00:00:00</h1> */}
+      <TimerInput />
       <Button>Start</Button>
       <style jsx>{`
         .root {
