74bed3b25ee17bcee8425b37f6f5dde698ba143b
coding-z <112389645+coding-z@users.noreply.github.com>
2024-12-10 18:01:11 +1000

    Update layout and menu

--- app/page.tsx
+++ app/page.tsx
@@ -15,65 +15,9 @@ export default function Page() {
   
   return (
     <>
-      <div id="root">
-        <header>
-          <button
-            className={page === Features.LISTS && "active"}
-            onClick={() => setPage(Features.LISTS)}
-          >
-            <GoChecklist size={40} />
-          </button>
-          <button
-            className={page === Features.TIMERS && "active"}
-            onClick={() => setPage(Features.TIMERS)}
-          >
-            <GoClock size={40} />
-          </button>
-          <button>
-            <CiCalendar size={40} />
-          </button>
-        </header>
-      </div>
+      <h1>Home</h1>
       <style jsx>{`
-        #root {
-          flex-grow: 1;
-          width: 100%;
-          display: block flex;
-          flex-direction: row;
-          justify-content: center;
-          align-items: center;
-        }
-
-        header {
-          position: absolute;
-          top: 50px;
-          left: 50px;
-          display: block flex;
-          flex-direction: row;
-          justify-content: space-between;
-          align-items: stretch;
-          gap: 10px;
-          background-color: rgb(255 255 255 / 0.1);
-          backdrop-filter: blur(20px);
-          padding: 10px;
-          border-radius: 5px;
-          border: 1px solid rgb(255 255 255 / 0.2);
-          box-shadow:
-            5px 5px 20px rgb(0 0 0 / 0.1),
-            inset 5px 5px 20px rgb(255 255 255 / 0.1);
-        }
-
-        main {
-          background-color: rgb(255 255 255 / 0.1);
-          backdrop-filter: blur(20px);
-          padding: 30px;
-          border-radius: 5px;
-          border: 1px solid rgb(255 255 255 / 0.2);
-          box-shadow:
-            5px 5px 20px rgb(0 0 0 / 0.1),
-            inset 5px 5px 20px rgb(255 255 255 / 0.1);
-          width: 500px;
-        }
+        
       `}</style>
     </>
   );
--- app/template.tsx
+++ app/template.tsx
@@ -1,11 +1,32 @@
 "use client";
 
+import Link from "next/link";
 import React from "react";
+import { CiCalendar } from "react-icons/ci";
+import { GoChecklist, GoClock, GoHome } from "react-icons/go";
 
 export default function Template({ children }: { children: React.ReactNode }) {
   return (
     <>
-      {children}
+      <div id="root">
+        <header>
+          <Link href="/" className="button">
+            <GoHome size={40} />
+          </Link>
+          <Link href="/lists" className="button">
+            <GoChecklist size={40} />
+          </Link>
+          <Link href="/timers" className="button">
+            <GoClock size={40} />
+          </Link>
+          <Link href="#" className="button">
+            <CiCalendar size={40} />
+          </Link>
+        </header>
+        <main>
+          {children}
+        </main>
+      </div>
       <style jsx global>{`
         html {
           box-sizing: border-box;
@@ -13,6 +34,46 @@ export default function Template({ children }: { children: React.ReactNode }) {
           *, *::before, *::after {
             box-sizing: inherit;
           }
+        }
+
+        #root {
+          flex-grow: 1;
+          width: 100%;
+          display: block flex;
+          flex-direction: row;
+          justify-content: center;
+          align-items: center;
+        }
+
+        header {
+          position: absolute;
+          top: 50px;
+          left: 50px;
+          display: block flex;
+          flex-direction: row;
+          justify-content: space-between;
+          align-items: stretch;
+          gap: 10px;
+          background-color: rgb(255 255 255 / 0.1);
+          backdrop-filter: blur(20px);
+          padding: 10px;
+          border-radius: 5px;
+          border: 1px solid rgb(255 255 255 / 0.2);
+          box-shadow:
+            5px 5px 20px rgb(0 0 0 / 0.1),
+            inset 5px 5px 20px rgb(255 255 255 / 0.1);
+        }
+
+        main {
+          background-color: rgb(255 255 255 / 0.1);
+          backdrop-filter: blur(20px);
+          padding: 30px;
+          border-radius: 5px;
+          border: 1px solid rgb(255 255 255 / 0.2);
+          box-shadow:
+            5px 5px 20px rgb(0 0 0 / 0.1),
+            inset 5px 5px 20px rgb(255 255 255 / 0.1);
+          width: 500px;
         }
 
         body {
@@ -52,7 +113,7 @@ export default function Template({ children }: { children: React.ReactNode }) {
           margin: 0;
         }
 
-        button {
+        button, .button {
           display: block flex;
           flex-direction: row;
           justify-content: center;
